dd143818ef1b62fd2399aecfa2175fa0b31adec4
lietou1986 <lietou1986@qq.com>
2017-05-16 16:52:58 +0800

    add CacheManagerExtensions

--- Dorado/Core/SuperCache/ICacheManager.cs
+++ Dorado/Core/SuperCache/ICacheManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace Dorado.Core.SuperCache
 {
@@ -7,5 +8,26 @@ namespace Dorado.Core.SuperCache
         TResult Get<TKey, TResult>(TKey key, Func<AcquireContext<TKey>, TResult> acquire);
 
         ICache<TKey, TResult> GetCache<TKey, TResult>();
+    }
+
+    public static class CacheManagerExtensions
+    {
+        private static readonly ConcurrentDictionary<object, object> _locks = new ConcurrentDictionary<object, object>();
+
+        public static TResult Get<TKey, TResult>(this ICacheManager cacheManager, TKey key, bool preventConcurrentCalls, Func<AcquireContext<TKey>, TResult> acquire)
+        {
+            if (preventConcurrentCalls)
+            {
+                var lockKey = _locks.GetOrAdd(key, _ => new object());
+                lock (lockKey)
+                {
+                    return cacheManager.Get(key, acquire);
+                }
+            }
+            else
+            {
+                return cacheManager.Get(key, acquire);
+            }
+        }
     }
 }
