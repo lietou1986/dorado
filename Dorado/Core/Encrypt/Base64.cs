b57ee11f46db7b05a157404534e4f86ac860bd06
jiangsongzhang <lietou1986@msn.com>
2018-11-05 18:37:14 +0800

    add base64

---
+++ Dorado/Core/Encrypt/Base64.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorado.Core.Encrypt
+{
+    public sealed class Base64
+    {
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="codeName">加密采用的编码方式</param>
+        /// <param name="source">待加密的明文</param>
+        /// <returns></returns>
+        public static string EncodeBase64(Encoding encode, string source)
+        {
+            byte[] bytes = encode.GetBytes(source);
+            try
+            {
+                return Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                return source;
+            }
+        }
+
+        /// <summary>
+        /// Base64加密，采用utf8编码方式加密
+        /// </summary>
+        /// <param name="source">待加密的明文</param>
+        /// <returns>加密后的字符串</returns>
+        public static string EncodeBase64(string source)
+        {
+            return EncodeBase64(Encoding.UTF8, source);
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="codeName">解密采用的编码方式，注意和加密时采用的方式一致</param>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string DecodeBase64(Encoding encode, string result)
+        {
+            byte[] bytes = Convert.FromBase64String(result);
+            try
+            {
+                return encode.GetString(bytes);
+            }
+            catch
+            {
+                return result;
+            }
+
+        }
+
+        /// <summary>
+        /// Base64解密，采用utf8编码方式解密
+        /// </summary>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string DecodeBase64(string result)
+        {
+            return DecodeBase64(Encoding.UTF8, result);
+        }
+
+    }
+
+}
